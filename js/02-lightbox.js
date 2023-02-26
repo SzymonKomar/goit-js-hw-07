362c379480464ccf8d72e4d6e039d476c463a9d5
SzymonKomar <114801537+SzymonKomar@users.noreply.github.com>
2023-02-26 22:08:54 +0100

    task-2/OnTheWay

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 // Change code below this line
+const gallerySelect = document.querySelector(".gallery");
+console.log(gallerySelect);
+const imagesToAdd = galleryItems
+  .map(
+    (galleryItem) =>
+      `<div class ="gallery__item"><a class ="gallery__link" href ="${galleryItem.original}"><img class= "gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></div>`
+  )
+  .join("");
+gallerySelect.insertAdjacentHTML("beforeend", imagesToAdd);
